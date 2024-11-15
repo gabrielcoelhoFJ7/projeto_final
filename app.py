5a4ce5fd2a223f4dab1bba880939282ea3dbec3f
Gabriel <microwaycs1@gmail.com>
2024-11-15 11:49:59 -0300

    projeto final

--- app.py
+++ app.py
@@ -15,8 +15,50 @@ def home():
 
 @app.route('/dashboard', methods=['GET'])
 def dashboard():
+    produtos_por_pagina = 15
+    # Obter o número da página a partir da query string (padrão: 1)
+    pagina_atual = int(request.args.get('pagina', 1))
+    print(pagina_atual)
+    # Calcular o offset
+    offset = (pagina_atual - 1) * produtos_por_pagina
+
+    # Selecionar os produtos com limite e offset
+    lista_produtos = (select(Produto, Categoria)
+                      .join(Categoria, Categoria.id_categoria == Produto.id_categoria)
+                      .offset(offset).limit(produtos_por_pagina))
+    lista_produtos = db_session.execute(lista_produtos).fetchall()
+
+    total_veterinarios = db_session.query(Produto).count()
+    total_paginas = (total_veterinarios + produtos_por_pagina - 1) // produtos_por_pagina
+
+    return render_template('dashboard.html',
+                           cavalo=lista_produtos,
+                           pagina_atual=pagina_atual,
+                           total_paginas=total_paginas
+                           )
 @app.route('/funcionario', methods=['GET'])
 def funcionario():
+    produtos_por_pagina = 15
+    # Obter o número da página a partir da query string (padrão: 1)
+    pagina_atual = int(request.args.get('pagina', 1))
+    print(pagina_atual)
+    # Calcular o offset
+    offset = (pagina_atual - 1) * produtos_por_pagina
+
+    # Selecionar os produtos com limite e offset
+    lista_produtos = (select(Produto, Categoria)
+                      .join(Categoria, Categoria.id_categoria == Produto.id_categoria)
+                      .offset(offset).limit(produtos_por_pagina))
+    lista_produtos = db_session.execute(lista_produtos).fetchall()
+
+    total_veterinarios = db_session.query(Produto).count()
+    total_paginas = (total_veterinarios + produtos_por_pagina - 1) // produtos_por_pagina
+
+    return render_template('funcionario.html',
+                           cavalo=lista_produtos,
+                           pagina_atual=pagina_atual,
+                           total_paginas=total_paginas
+                           )
 
 
 @app.route('/produto', methods=['GET'])
